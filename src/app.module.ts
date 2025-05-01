4ace3781d675cc67d288e51edbc93af1c8344fb0
shahsuresh <shahsuresh75@gmail.com>
2025-05-01 22:45:28 +0545

    teacher module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatagoryModule } from './catagory/catagory.module';
 import { StudentModule } from './student/student.module';
+import { TeacherController } from './teacher/teacher.controller';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [CatagoryModule, StudentModule], // CatagoryModule automatically imported by nestjs if new module is created
-  controllers: [AppController],
+  imports: [CatagoryModule, StudentModule, TeacherModule], // CatagoryModule automatically imported by nestjs if new module is created
+  controllers: [AppController, TeacherController],
   providers: [AppService],
 })
 export class AppModule {}
